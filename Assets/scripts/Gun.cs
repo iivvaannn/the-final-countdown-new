29386cb235a67b3d7c161a4b64c347138c395c68
iivvaannn <167764581+iivvaannn@users.noreply.github.com>
2025-10-10 14:05:22 +0200

    gun code 2

--- Assets/scripts/Gun.cs
+++ Assets/scripts/Gun.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float damage = 10f;
+    public float range = 100f;
 
-    // Update is called once per frame
+    public Camera fpsCam;
+
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            shoot();
+        }
+    }
+
+
+    void shoot()
+    {
+        RaycastHit hit;
+       if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
     }
 }
