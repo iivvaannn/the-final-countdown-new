e814c0ab0e9767eb8f180a2696a3e6afb81b2bfe
arslan-samsak <arslan.samsak@elev.ga.klaragymnasium.se>
2025-11-14 12:48:31 +0100

    kommentera impacteffect
    kommentera impacteffect

--- Assets/scripts/Gun.cs
+++ Assets/scripts/Gun.cs
@@ -10,7 +10,7 @@ public class Gun : MonoBehaviour
 
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
-    public GameObject impactEffect;
+ //   public GameObject impactEffect;
 
     public float nextTimeToFire = 0f;
 
@@ -83,8 +83,8 @@ public class Gun : MonoBehaviour
                 hit.rigidbody.AddForce(-hit.normal * impactForce);
             }
 
-            GameObject impactOG = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
-            Destroy(impactOG, 2f);
+          //  GameObject impactOG = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            //Destroy(impactOG, 2f);
         }
     }
 
